082b93fc7ff803c9ab8f66e159434d141f15e6b0
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-31 21:06:18 +0200

    >

--- monthly_challanges/challenges/views.py
+++ monthly_challanges/challenges/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+monthly_challenges = {
+    "january": "Eat no meat!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Do Yoga!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Do Yoga!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Do Yoga!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": "Learn Django for at least 20 minutes every day!"
+}
 
 # Create your views here.
 
 
-def monthly_challenge(request, month):
-    challenge_text = ""
-    if month == "january":
-        challenge_text = "Eat no meat!"
-    elif month == "february":
-        challenge_text = "Walk for at least 20 minutes every day!"
-    elif month == "march":
-        challenge_text = "Learn Django for at least 20 minutes every day!"
-    elif month == "april":
-        challenge_text = "Eat no meat!"
-    elif month == "may":
-        challenge_text = "Walk for at least 20 minutes every day!"
-    elif month == "june":
-        challenge_text = "Learn Django for at least 20 minutes every day!"
+def monthly_challange_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid Month")
+    redirect_month = months[month-1]
+    return HttpResponseRedirect("/challanges/" + redirect_month)
 
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound("This page is not suported!")
     return HttpResponse(challenge_text)
