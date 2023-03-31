c479016f08c941191c263632f4693b7dac19f57a
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-31 21:47:48 +0200

    .

--- monthly_challanges/challenges/views.py
+++ monthly_challanges/challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "Eat no meat!",
@@ -19,17 +20,32 @@ monthly_challenges = {
 # Create your views here.
 
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        month_path = reverse("month-challange", args=[month])
+        list_items += f"<li><a href='{month_path}'>{month.capitalize()}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+
+    return HttpResponse(response_data)
+
+
 def monthly_challange_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
         return HttpResponseNotFound("Invalid Month")
     redirect_month = months[month-1]
-    return HttpResponseRedirect("/challanges/" + redirect_month)
+    redirect_path = reverse("month-challange", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This page is not suported!")
-    return HttpResponse(challenge_text)
+        return HttpResponseNotFound("<h1>This page is not suported!</h1>")
