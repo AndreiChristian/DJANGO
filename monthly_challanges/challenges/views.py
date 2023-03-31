5ca7e27d7b3cf2c4488631d70015dcf3579ddc1e
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-31 20:42:00 +0200

    more and more

--- monthly_challanges/challenges/views.py
+++ monthly_challanges/challenges/views.py
@@ -3,5 +3,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This works")
+
+def monthly_challenge(request, month):
+    challenge_text = ""
+    if month == "january":
+        challenge_text = "Eat no meat!"
+    elif month == "february":
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif month == "march":
+        challenge_text = "Learn Django for at least 20 minutes every day!"
+    elif month == "april":
+        challenge_text = "Eat no meat!"
+    elif month == "may":
+        challenge_text = "Walk for at least 20 minutes every day!"
+    elif month == "june":
+        challenge_text = "Learn Django for at least 20 minutes every day!"
+
+    return HttpResponse(challenge_text)
