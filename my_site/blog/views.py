33ad50729305265c96d113d3daa8173a9f429708
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-13 18:28:05 +0300

    added basic templates, connected the app in settings

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+def starting_page(request):
+    return HttpResponse("Home page")
+
+
+def posts(requst):
+    return HttpResponse("Posts page")
+
+
+def posts_detail(request):
+    return HttpResponse("Posts detail page")
