9c7f8e3b6351066b86d80e611ddc4346819ed820
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-14 11:41:08 +0300

    added one more python project

---
+++ rest_tutorial/books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Book(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True)
+    price = models.IntegerField()
+    description = models.TextField()
+    author = models.TextField()
+    inStock = models.BooleanField(default=True)
+    genre = models.CharField(max_length=100)
--- rest_tutorial/rest_tutorial/settings.py
+++ rest_tutorial/rest_tutorial/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'books',
     'snippets',
     'django.contrib.admin',
     'django.contrib.auth',
