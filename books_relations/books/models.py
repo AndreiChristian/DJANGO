138a31ef3817c0221e7dc9094937e5d2d41dc73f
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-24 21:15:45 +0200

    the serializer kind of works

--- books_relations/books/models.py
+++ books_relations/books/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Category model, each category has multiple books
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
@@ -8,11 +10,14 @@ class Category(models.Model):
         return self.name
 
 # Book model, each book belongs to one category
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     # ForeignKey creates a one-to-many relationship between Category and Book
-    category = models.ForeignKey(Category, related_name='books', on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name='books', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- books_relations/books/views.py
+++ books_relations/books/views.py
@@ -34,8 +34,24 @@ def category_list(request):
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
 
-    if request.method == "POST":
+    elif request.method == "POST":
         serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "POST"])
+def book_list(request):
+
+    if request.method == "GET":
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = BookSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
