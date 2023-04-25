6bc6dfe6756693a585ee0e1926ecf9a7645a30d4
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-04-25 18:36:49 +0200

    it works

--- books_relations/properties/models.py
+++ books_relations/properties/models.py
@@ -6,21 +6,28 @@ from django.db import models
 class FacilityCategory(models.Model):
     name = models.CharField(max_length=100)
 
-
+    def __str__(self):
+        return self.name
 class FacilitySubCategory(models.Model):
     name = models.CharField(max_length=100)
     category = models.ForeignKey(
         FacilityCategory, on_delete=models.CASCADE, related_name='subcategories')
 
-
+    def __str__(self):
+        return self.name
 class FacilityItem(models.Model):
     name = models.CharField(max_length=100)
     subcategory = models.ForeignKey(
         FacilitySubCategory, on_delete=models.CASCADE, related_name='items')
 
+    def __str__(self):
+        return self.name
 
 class Property(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     square_meters = models.IntegerField()
     facility_categories = models.ManyToManyField(FacilityCategory)
+
+    def __str__(self):
+        return self.name
--- books_relations/properties/serializers.py
+++ books_relations/properties/serializers.py
@@ -1,33 +1,54 @@
 from rest_framework import serializers
 from .models import Property, FacilityCategory, FacilityItem, FacilitySubCategory
 
+
 class FacilityItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = FacilityItem
-        fields = ['id', 'name']
+        fields = ['id', 'name','subcategory']
+
 
 class FacilitySubCategorySerializer(serializers.ModelSerializer):
-    items = FacilityItemSerializer(many=True, read_only=True)
+    items = serializers.PrimaryKeyRelatedField(
+        queryset=FacilityItem.objects.all(), many=True)
 
     class Meta:
         model = FacilitySubCategory
-        fields = ['id', 'name', 'items']
+        fields = ['id', 'name', 'items', 'category']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['items'] = FacilityItemSerializer(
+            instance.items.all(), many=True).data
+        return representation
+
 
 class FacilityCategorySerializer(serializers.ModelSerializer):
-    subcategories = FacilitySubCategorySerializer(many=True, read_only=True)
+    subcategories = serializers.PrimaryKeyRelatedField(
+        queryset=FacilitySubCategory.objects.all(), many=True)
 
     class Meta:
         model = FacilityCategory
         fields = ['id', 'name', 'subcategories']
 
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['subcategories'] = FacilitySubCategorySerializer(
+            instance.subcategories.all(), many=True).data
+        return representation
+
+
 class PropertySerializer(serializers.ModelSerializer):
-    facility_categories = serializers.PrimaryKeyRelatedField(queryset=FacilityCategory.objects.all(), many=True)
+    facility_categories = serializers.PrimaryKeyRelatedField(
+        queryset=FacilityCategory.objects.all(), many=True)
 
     class Meta:
         model = Property
-        fields = ['id', 'name', 'location', 'square_meters', 'facility_categories']
+        fields = ['id', 'name', 'location',
+                  'square_meters', 'facility_categories']
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        representation['facility_categories'] = FacilityCategorySerializer(instance.facility_categories.all(), many=True).data
+        representation['facility_categories'] = FacilityCategorySerializer(
+            instance.facility_categories.all(), many=True).data
         return representation
